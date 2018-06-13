8aa47a5d0ca21ae2b80ae6088081d84e56c5cc31
nandavikas <34337075+nandavikas@users.noreply.github.com>
2018-06-13 16:57:14 +0530

    Update svm_author_id.py

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,6 +24,9 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+clf = svm.SVC()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
 #########################################################
 
